cbe173c26e96e5753b811bb45b59efe1af8e8c73
pizzacat83 <17941141+pizzacat83@users.noreply.github.com>
2019-02-10 22:27:07 +0900

    implement rendering of waveform and pitch

--- index.js
+++ index.js
@@ -1,10 +1,53 @@
 'use strict';
+async function get(url, responseType='text'){
+    return new Promise((resolve, reject)=>{
+        let xhr = new XMLHttpRequest();
+        xhr.responseType =  responseType;//'arraybuffer';
+        xhr.open('GET', url, true);
+        xhr.onload = ()=>{
+            if(xhr.status == 200){
+                resolve(xhr.response);
+            } else {
+                reject(Error(xhr.statusText));
+            }
+        };
+        xhr.onerror = ()=>{reject(Error('network error'));};
+        xhr.send();
+    });
+}
 
-var wavesurfer = WaveSurfer.create({
-    container: '#waveform',
-    waveColor: 'darkturquoise',
+async function drawPitch(path, container){
+    const res=await get(path.replace('.','_')+'.frq');
+    const frq=JSON.parse(res);
+    const canvas=container.getElementsByTagName('wave')[0].getElementsByTagName('canvas')[0].cloneNode(false);
+    canvas.style['z-index']='4';
+    container.getElementsByTagName('wave')[0].appendChild(canvas);
+    const ctx = canvas.getContext('2d');
+    ctx.strokeStyle='hotpink';
+    ctx.lineWidth=2;
+    ctx.beginPath();
+    for(let i=0;i<frq.length;++i){
+        ctx.lineTo((50+i)/(frq.length+50)*canvas.width, canvas.height/2-frq[i]*10);
+    }
+    ctx.closePath();
+    ctx.stroke();
+}
+
+
+
+const dstwaveform = WaveSurfer.create({
+    container: '#dstwaveform',
+    waveColor: 'turquoise',
     progressColor: 'lightseagreen',
     barWidth: 2
 });
-var path = 'voicebanks/dst/_ああいあう↓.wav';
-wavesurfer.load(path);
+let dstpath = 'voicebanks/dst/_ああいあう↓.wav';
+
+async function displayDstWave(path, container){
+    dstwaveform.load(path);
+    setTimeout(()=>{drawPitch(path, container);}, 800); // 描画終了を待つ
+}
+
+displayDstWave(dstpath, dstwaveform.container);
+
+
